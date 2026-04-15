b3dcdf521f881523d74c2cf4c184d4e9159c8942
print-keer <keerthimanoja.7@gmail.com>
2026-04-15 21:12:40 +0530

    commit2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+// hosting
 export default defineConfig({
   base: "/Portfolio/",
   plugins: [react(), tailwindcss()],
